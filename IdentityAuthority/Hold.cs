8a7d87af629c3d709d15ed082a94000c09c303c7
Gino Arpesella <ginoarpesella@gmail.com>
2018-07-18 00:07:52 +0100

    Add project files.

---
+++ IdentityAuthority/Hold.cs
@@ -0,0 +1,94 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityAuthority
+{
+    internal class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client> {
+                new Client {
+                ClientId = "oauthClient",
+                ClientName = "Example Client Credentials Client Application",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = new List<Secret> {
+                    new Secret("superSecretPassword".Sha256())},
+                AllowedScopes = new List<string> {"customAPI.read"}
+            },
+            new Client {
+                ClientId = "openIdConnectClient",
+                ClientName = "Example Implicit Client Application",
+                AllowedGrantTypes = GrantTypes.Implicit,
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "role",
+                    "customAPI.write"
+                },
+                RedirectUris = new List<string> { "https://localhost:44395/signin-oidc" }, 
+                PostLogoutRedirectUris = new List<string> { "https://localhost:44396" }
+            }};
+        }
+    }
+
+    internal class Resources
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource> {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource {
+                    Name = "role",
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource> {
+                new ApiResource {
+                    Name = "customAPI",
+                    DisplayName = "Custom API",
+                    Description = "Custom API Access",
+                    UserClaims = new List<string> {"role"},
+                    ApiSecrets = new List<Secret> {new Secret("scopeSecret".Sha256())},
+                    Scopes = new List<Scope> {
+                        new Scope("customAPI.read"),
+                        new Scope("customAPI.write")
+                    }
+                }
+            };
+        }
+    }
+
+    internal class Users
+    {
+        public static List<TestUser> Get()
+        {
+            return new List<TestUser> {
+                new TestUser {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "gino",
+                    Password = "password123",
+                    Claims = new List<Claim> {
+                        new Claim(JwtClaimTypes.Email, "gion@7layer.net"),
+                        new Claim(JwtClaimTypes.Role, "admin")
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ IdentityAuthority/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IdentityAuthority
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddIdentityServer()
+                .AddInMemoryClients(Clients.Get())
+                .AddInMemoryIdentityResources(Resources.GetIdentityResources())
+                .AddInMemoryApiResources(Resources.GetApiResources())
+                .AddTestUsers(Users.Get())
+                .AddDeveloperSigningCredential();
+
+            services.AddAuthentication()
+            .AddGoogle("Google", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.ClientId = "281475367621-1i0fn8jeiurhgjm4uveg8bq4na85qbpv.apps.googleusercontent.com"; // https://console.developers.google.com go create your own
+                        options.ClientSecret = "n9BtAuOOYetv6Vbh1il9BjYO";
+                })
+                .AddOpenIdConnect("oidc", "IdentityServer", options =>
+                    {
+                        options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                        options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+
+                        options.Authority = "https://localhost:44395/";
+                        options.ClientId = "implicit";
+                        options.ResponseType = "id_token";
+                        options.SaveTokens = true;
+                        options.CallbackPath = new PathString("/signin-idsrv");
+                        options.SignedOutCallbackPath = new PathString("/signout-callback-idsrv");
+                        options.RemoteSignOutPath = new PathString("/signout-idsrv");
+
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            NameClaimType = "name",
+                            RoleClaimType = "role"
+                        };
+                    });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("We are live!");
+            });
+        }
+    }
+}
